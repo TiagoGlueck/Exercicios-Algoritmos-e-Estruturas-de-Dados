dea3ec646dd9770c34ffaf292c2b4dd12fd14db2
Tiago da Gloria Glueck <gloriagluecK@outlook.com>
2025-02-24 14:08:04 -0300

    Correcao Exercicios concluidos

--- exercicio1.c
+++ exercicio1.c
@@ -14,31 +14,31 @@ int main()
     scanf("%f", &nota2);
     scanf("%f", &nota3);
     scanf("%f", &nota4);
-    printf("A media das suas quatro notas são de: %f", (nota1+nota2+nota3+nota4)/4);*/
+    printf("A media das suas quatro notas sao de: %.2f", (nota1+nota2+nota3+nota4)/4);*/
 
     /*Exercicio3
     float valor1,valor2;
     printf("Digite dois numeros\n");
     scanf("%f", &valor1);
     scanf("%f", &valor2);
-    printf("A soma e de: %f \n", (valor1+valor2));
-    printf("O produto e de: %f \n", (valor1*valor2));
-    printf("A diferenca e de: %f \n", (valor1-valor2));
-    printf("O quociente e de: %f \n", (valor1/valor2));*/
+    printf("A soma e de: %.2f \n", (valor1+valor2));
+    printf("O produto e de: %.2f\n", (valor1*valor2));
+    printf("A diferenca e de: %.2f \n", (valor1-valor2));
+    printf("O quociente e de: %.2f \n", (valor1/valor2));*/
 
     /*Exercicio4
     float fare;
     printf("Insira quantos graus farenheit deseja converter para centigrados: \n");
     scanf("%f", &fare);
     float c= (fare-32)*5/9;
-    printf("A temperatura em centigrados e: %f\n", c);*/
+    printf("A temperatura em centigrados sao: %.2f\n", c);*/
 
     /*Exercicio5
-    float horas;
+    int horas;
     printf("Quantas horas se passaram?");
-    scanf("%f", &horas);
-    float segundos = horas*3600;
-    printf("Se passaram: %f", segundos);*/
+    scanf("%d", &horas);
+    int segundos = horas*3600;
+    printf("Se passaram: %d", segundos);*/
 
     /*Exercicio6
     int p1 = 2, p2 = 1, p3 = 3, p4 = 4;
@@ -49,7 +49,7 @@ int main()
     scanf("%f", &valor3);
     scanf("%f", &valor4);
     float media = (valor1*p1 + valor2*p2 + valor3*p3 + valor4*p4) / (p1 + p2 + p3 + p4);
-    printf("Sua media ponderada e de: %f", media);*/
+    printf("Sua media ponderada e de: %.2f", media);*/
 
     return 0;
 }
