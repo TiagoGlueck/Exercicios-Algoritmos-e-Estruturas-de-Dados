c92077b9c68ad217198c530f8dd9b6b3fb480ba3
Tiago da Gloria Glueck <gloriagluecK@outlook.com>
2025-02-25 10:14:33 -0300

    Correcao 2 exercicios

--- exercicio1.c
+++ exercicio1.c
@@ -10,17 +10,13 @@ int main()
     /*Exercicio2
     float nota1,nota2,nota3,nota4;
     printf("Digite suas quatro notas\n");
-    scanf("%f", &nota1);
-    scanf("%f", &nota2);
-    scanf("%f", &nota3);
-    scanf("%f", &nota4);
+    scanf("%f %f %f %f", &nota1, &nota2, &nota3, &nota4);
     printf("A media das suas quatro notas sao de: %.2f", (nota1+nota2+nota3+nota4)/4);*/
 
     /*Exercicio3
     float valor1,valor2;
     printf("Digite dois numeros\n");
-    scanf("%f", &valor1);
-    scanf("%f", &valor2);
+    scanf("%f %f", &valor1, &valor2 );
     printf("A soma e de: %.2f \n", (valor1+valor2));
     printf("O produto e de: %.2f\n", (valor1*valor2));
     printf("A diferenca e de: %.2f \n", (valor1-valor2));
@@ -44,10 +40,7 @@ int main()
     int p1 = 2, p2 = 1, p3 = 3, p4 = 4;
     float valor1, valor2, valor3, valor4;
     printf("Insira quatro valores:\n");
-    scanf("%f", &valor1);
-    scanf("%f", &valor2);
-    scanf("%f", &valor3);
-    scanf("%f", &valor4);
+    scanf("%f %f %f %f", &valor1, &valor2, &valor3, &valor4);
     float media = (valor1*p1 + valor2*p2 + valor3*p3 + valor4*p4) / (p1 + p2 + p3 + p4);
     printf("Sua media ponderada e de: %.2f", media);*/
 
